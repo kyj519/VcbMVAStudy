b09186bc8c036fb89c9c77ea54923894f0cc2d68
yeonjoon kim <yeonjoon.kim@cern.ch>
2023-02-27 12:43:20 +0900

    ~

--- TabNet_Permutation/MVA.py
+++ TabNet_Permutation/MVA.py
@@ -1,48 +1,55 @@
-import ROOT
 import os, sys
 import numpy as np
-#######
-#BELOW CODE IS OPTIMIZED FOR TENSORFLOW-2.4.1!
-#######
+print(np.__file__)
 from pytorch_tabnet.tab_model import TabNetClassifier
+
 import torch
+print(torch.__file__)
+
 import sys
 sys.path.append(os.environ["DIR_PATH"])
 from root_data_loader import load_data, classWtoSampleW
 print(torch.cuda.is_available())
-print(torch.cuda.get_device_name(0))
+#print(torch.cuda.get_device_name(0))
 print(torch.cuda.device_count())
-
-ROOT.EnableImplicitMT()
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
 
-varlist_prekin = ['had_t_b_pt','w_u_pt','w_d_pt','lep_t_b_pt','had_t_b_bscore','lep_t_b_bscore',
+
+varlist = varlist_prekin = ['pt_had_t_b','pt_w_u','pt_w_d','pt_lep_t_b','bvsc_had_t_b','bvsc_lep_t_b',
    'theta_w_u_w_d','theta_lep_neu','theta_lep_w_lep_t_b', 'del_phi_had_t_lep_t',
-   'had_t_mass','had_w_mass','lep_t_mass','lep_t_partial_mass']
-varlist = varlist_prekin + ['chi2']
+   'had_t_mass','had_w_mass','lep_t_mass','lep_t_partial_mass','pt_ratio','chi2','weight']
   
 if __name__ == '__main__':
 
   
   
   path_sample = os.environ["WtoCB_PATH"]
-  #filename = 'Vcb_Mu_TTLJ_WtoCB_powheg_25.root'
   filename = 'Vcb_Mu_TTLJ_WtoCB_powheg.root'
- 
+  #,'Reco_41','Reco_23','Reco_21'
   data =  load_data(os.path.join(path_sample,filename), 'n_jets==4',varlist,0.1,0.2,['Permutation_Correct'],['Permutation_Wrong'])
-  clf = TabNetClassifier()  #TabNetRegressor()
+  print("file loaded")
+  print(data['cat_idxs'],data['cat_dims']) 
+  clf = TabNetClassifier(
+    verbose=1,
+    cat_idxs=data['cat_idxs'],
+    cat_dims=data['cat_dims'],
+    cat_emb_dim=6
+    )  
   clf.fit(
-    data['train_features'],data['train_y'],
+    X_train=data['train_features'],y_train=data['train_y'],
     eval_set=[(data['val_features'], data['val_y'])],
     eval_metric=['auc'],
-    weights=data['class_weight']
+    #weights=data['class_weight']
+    weights=1,
+    batch_size=512,
+    patience=20
+    #callbacks=[pytorch_tabnet.callbacks.History(clf,verbose=1)]
   )
-  preds = clf.predict(data['test_features'])
-  clf.save_model('/data6/Users/yeonjoon/VcbMVAStudy/TabNet_Permutation/')
+  clf.save_model('/data6/Users/yeonjoon/VcbMVAStudy/TabNet_Permutation/model.pt')
   
   
 
