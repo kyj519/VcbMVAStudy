44ef293bfdbecc5262425a878d53e64a5268d90f
yeonjoon kim <yeonjoon.kim@cern.ch>
2023-05-11 01:20:08 +0900

    ~

--- TabNet_template/writeScore.sh
+++ TabNet_template/writeScore.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python MVA.py --working_mode infer --input_root_file $GV0/Vcb/Sample --input_model './model_KPS_balancedAccu/model_forKPS_metricBalancedAccuracy.pt.zip'
+dir=$DIR_PATH
+method='TabNet_template'
+#root -l -b -q "${dir}/$method/Permutation_MVA.cxx($n_jet, $pre_kin)"
+python3 "${dir}/$method/MVA.py --working_mode infer --input_root_file $GV0/Vcb/Sample --input_model './model_KPS_balancedAccu/model_forKPS_metricBalancedAccuracy.pt.zip'
